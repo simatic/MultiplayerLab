f3905a93b5850786421a84501fbf255efe114fbb
Malan-Tai <malantai99@gmail.com>
2020-07-31 02:01:10 +0200

    Added drifting and drag

--- 2M3/Source/Source.cpp
+++ 2M3/Source/Source.cpp
@@ -33,16 +33,29 @@ int main()
 	sf::Clock clock;
 
     sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
-	sf::RectangleShape shape(sf::Vector2f(100, 50));
-	shape.setOrigin(sf::Vector2f(50, 25));
+	sf::RectangleShape shape(sf::Vector2f(40, 20));
+	shape.setOrigin(sf::Vector2f(20, 10));
 
 	sf::Vector2f carPos(800, 450);
 	shape.setPosition(carPos);
 	sf::Vector2f carDirection(1, 0);
 	sf::Vector2f carSpeed(0, 0);
+	float carMaxSpeed = 1000;
 	float carAngle = 0;
+	float carAccel = 200;
 
-	float R = 50;
+	float R = 12;
+
+	bool forward = true;
+	bool drifting = false;
+	float prevDriftingSign = 0;
+	float driftThreshold = carMaxSpeed - 200;
+	float driftAngle = M_PI / 3;
+
+	float drag = 0.001;
+
+	sf::VertexArray tires(sf::Lines, 1);
+	tires[0].position = carPos;
 
     while (window.isOpen())
     {
@@ -55,24 +68,34 @@ int main()
 			window.close();
 		}
 
+		float l = length(carSpeed);
 		float accel = 0;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 		{
-			accel += 100;
+			float f = 1;
+			if (!forward) f = 10;
+			accel += f * carAccel;
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 		{
-			accel -= 100;
+			float f = 1;
+			if (forward) f = 10;
+			accel -= f * carAccel;
+		}
+		if (accel == 0)
+		{
+			accel = (l * l + 2 * l) * drag;
+			if (forward) accel *= -1;
 		}
 
 		float angle = 0;
 		float angleSign = 0;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && l > 50)
 		{
 			angle += M_PI / 3;
 			angleSign += 1;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && l > 50)
 		{
 			angle -= M_PI / 3;
 			angleSign -= 1;
@@ -83,10 +106,18 @@ int main()
 
 		sf::Vector2f tangAccelVector = tangAccel * carDirection;
 		carSpeed += tangAccelVector * dt.asSeconds();
+		l = length(carSpeed);
+		if (l > carMaxSpeed)
+		{
+			carSpeed *= carMaxSpeed / l;
+		}
+
+		bool prevDrifting = drifting;
+		drifting = forward && l > driftThreshold && angleSign != 0;
 
 		float theta = sqrt(abs(radAccel) / R) * dt.asSeconds();
-
-		if (scalar(carSpeed, carDirection) < 0)
+		forward = scalar(carSpeed, carDirection) >= 0;
+		if (!forward)
 		{
 			carSpeed = rotate(carSpeed, -theta * angleSign);
 			carDirection = rotate(carDirection, -theta * angleSign);
@@ -97,24 +128,39 @@ int main()
 			carDirection = rotate(carDirection, theta * angleSign);
 		}
 
+		if (prevDrifting && !drifting)
+		{
+			carDirection = rotate(carDirection, prevDriftingSign * driftAngle);
+			carSpeed = rotate(carSpeed, prevDriftingSign * driftAngle);
+		}
+		prevDriftingSign = angleSign;
+
 		carPos += carSpeed * dt.asSeconds();
 
-		if (carPos.x > 1600) carPos.x = 0;
+		/*if (carPos.x > 1600) carPos.x = 0;
 		else if (carPos.x < 0) carPos.x = 1600;
 
 		if (carPos.y > 900) carPos.y = 0;
 		else if (carPos.y < 0) carPos.y = 900;
 
-		shape.setPosition(carPos);
+		shape.setPosition(carPos);*/
 
 		carAngle = 0;
 		if (carDirection.x != 0) carAngle = -atan2(carDirection.y, carDirection.x);
 		if (carDirection.x == 0 && carDirection.y != 0) carAngle = M_PI_2 * carDirection.y / abs(carDirection.y);
-
+		if (drifting) carAngle += angleSign * driftAngle;
 		shape.setRotation(-carAngle * 180.0 / M_PI);
+
+		for (int i = 0; i < tires.getVertexCount(); i++)
+		{
+			tires[i].position -= carSpeed * dt.asSeconds();
+		}
+		tires.append(sf::Vertex(shape.getPosition() - (float)20 * carDirection));
+		tires.append(sf::Vertex(shape.getPosition() - (float)20 * carDirection));
 
 		window.clear();
 		window.draw(shape);
+		window.draw(tires);
 		window.display();
     }
 
