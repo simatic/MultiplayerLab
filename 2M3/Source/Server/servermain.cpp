1aef74de7e682eb3e50026d8fde9466a28a9b182
jglrxavpok <jglrxavpok@gmail.com>
2020-10-15 11:11:43 +0200

    Ajout de l'exécutable du serveur dans CMake

---
+++ 2M3/Source/Server/servermain.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jglrxavpok on 15/10/2020.
+//
+
+#include "Server/servermain.h"
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
