0304ba29b7e68c5f2813c714aba714a708690cbd
florianbsh <florian.bescher@telecom-sudparis.eu>
2021-01-08 15:47:58 +0100

    Added support for Component serialization.

--- 2M3/Include/Common/Components/Component.h
+++ 2M3/Include/Common/Components/Component.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <typeinfo>
+#include <iostream>
 
 struct Component
-{};
+{
+public:
+    friend std::ostream& operator<<(std::ostream& os, const Component& component) { return component.serialize(os); };
+    friend std::istream& operator>>(std::istream& is, Component& component) { return component.deserialize(is); };
+
+protected:
+    virtual std::ostream& serialize(std::ostream& os) const { return os; };
+    virtual std::istream& deserialize(std::istream& is) { return is; };
+};
 
 template <typename T>
 struct IdentifiableComponent : Component
