5f147acbfebb28457e5acbd5728082bb36891d96
florianbsh <florian.bescher@telecom-sudparis.eu>
2021-01-31 19:26:31 +0100

    Removed unused (legacy) folder

--- 2M3/Include/Renderable.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "ResourceIdentifiers.h"
-
-class Renderable
-{
-public:
-	virtual void draw(sf::RenderTarget& target) = 0;
-};
--- 2M3/Unused/Source/WallLogic.cpp
+++
@@ -1,8 +0,0 @@
-#include "WallLogic.h"
-
-WallLogic::WallLogic(sf::Vector2f position, sf::RectangleShape colliderShape) :
-    OldEntity(position, std::move(colliderShape)) {
-    getComponent<Collider>()->shape.setPosition(getComponent<Transform>()->position + getComponent<Collider>()->shape.getSize() / 2.0f);
-}
-
-void WallLogic::update(sf::Time dt, std::vector<OldEntity*> entities, std::vector<OldEntity*>& newEntities, std::set<Pair>& pairs) {}
